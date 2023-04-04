444c6e055961f0c76b199d6994327065a908cd6b
akanshkumar25 <112652858+akanshkumar25@users.noreply.github.com>
2023-04-03 21:21:41 -0700

    added headers and button to backlog page

--- src/component/Backlog.jsx
+++ src/component/Backlog.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import InteractiveList from './InteractiveList'
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const Wrapper = styled.div`
+
+.heading-bar {
+    display:flex;
+    justify-content: space-between;
+    margin-top:20px
+}
+.heading{
+    // margin-top: 10px;
+    color: #8C1D40;
+}
+
+.create-btn {
+    margin-top:10%;
+    background-color: #8C1D40;
+}
+
+img {
+    height:300px;
+    width: 500px;
+    opacity:0.1;
+    margin: 100px 200px;
+    position:absolute;
+}
+
+.project-list {
+    margin-top:30px;
+    a {
+        text-decoration:none;
+    }        
+}
+`;
 
 const Backlog = () => {
+    console.log("backlog page called")
+    const [showDialog, setShowDialog] = useState(false);
   return (
-    <InteractiveList />
+    <Wrapper>
+        <div className='heading-bar'>
+                <Typography className="heading" variant="h3" gutterBottom>
+                    Backlog items
+                </Typography>
+                <div>
+                    <Button className="create-btn" variant="contained" onClick={() => setShowDialog(true)}>Create User Story</Button>
+                </div>
+        </div>
+        <InteractiveList />
+    </Wrapper>
+    
   )
 }
 
