3074b7346ccf9a48b50ff69d01311e793f3e3917
naheer <nnolas33@asu.edu>
2023-04-14 22:07:09 -0700

    created the dialog box for adding roles

---
+++ src/component/CreateRoles.jsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import { Formik, Form } from 'formik';
+import TextField from '@mui/material/TextField';
+import { createUserstory } from '../apis/backlog';
+import Cookies from 'js-cookie';
+import Backlog from './Backlog';
+import { ColorRing } from 'react-loader-spinner';
+import { createRoles } from '../apis';
+
+
+const Wrapper = styled.div`
+
+height: 300px;
+width:600px;
+background-color: #f7f3f2;
+border: 2px solid #8C1D40;
+position:absolute;
+z-index:100;
+border-radius:20px;
+top:25%;
+left: 40%;
+
+.loaders {
+    height:30px;
+}
+
+.UserStory-form {
+    padding:20px;
+    display:flex;
+    flex-direction: column;
+    justify-content: space-between;
+
+    .heading {
+        color: #8C1D40;
+        margin-bottom: 40px;
+    }
+
+    .subject-field {
+        margin-bottom: 20px;
+    }
+    .desc-field {
+        margin-bottom: 40px;
+    }
+    .crt-btn {
+        background-color:#8C1D40;
+    }
+    .cancel-btn {
+        margin-top:20px;
+        background-color:grey;
+    }
+}
+
+`;
+
+const CreateRoles = ({ dialog, addRoles }) => {
+    let username = Cookies.get('username');
+    let password = Cookies.get('password');
+    let projectName = Cookies.get('projectName');
+    let projectId = Cookies.get('projectId')
+    const [isCreateLoader, setIsCreateLoader] = useState(false);
+
+    return (
+
+        <Wrapper>
+            <Formik
+                initialValues={{
+                    memberName: '',
+                    roleName:'',
+                }}
+                onSubmit={(values) => {
+                    CreateRoles(username,password,values.memberName,projectId,values.roleName)
+                    .then(res=>{
+                        dialog();
+                        addRoles(values.memberName,values.roleName)
+                    })
+                }}
+            >
+                {
+                    props => (
+                        <Form className="UserStory-form">
+                            <Typography className="heading" variant="h4" gutterBottom>Add Role</Typography>
+                            <TextField id="outlined-basic" className="subject-field" onChange={props.handleChange} label="Username" name="memberName" variant="outlined" />
+                            <TextField id="outlined-basic" className="subject-field" onChange={props.handleChange} label="Roles" name="roleName" variant="outlined" />
+                            <Button variant="contained" className="crt-btn" type="submit">
+                                {
+                                    isCreateLoader ?
+                                        <ColorRing
+                                            visible={true}
+                                            className="loader"
+                                            height="80"
+                                            width="80"
+                                            ariaLabel="blocks-loading"
+                                            wrapperClass="loaders"
+                                            colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
+                                        />
+                                        :
+                                        'Add'
+                                }
+                            </Button>
+                            <Button variant="contained" className="cancel-btn" onClick={dialog}>Cancel</Button>
+                        </Form>
+                    )
+                }
+            </Formik>
+        </Wrapper>
+    )
+}
+
+export default CreateRoles;
