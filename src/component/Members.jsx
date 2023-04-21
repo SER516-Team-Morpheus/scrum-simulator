825ffd7634a0d59db64800d345f831080478e392
Chenxi6132 <chenxix2@asu.edu>
2023-04-20 23:18:54 -0700

    update member page file

--- src/component/Members.jsx
+++ src/component/Members.jsx
@@ -184,39 +184,16 @@ const Members = () => {
                                 title=""
                                 columns={columns}
                                 data={memberList}
-                            // editable={{
-                            //     onRowAdd: newData =>
-                            //         new Promise((resolve, reject) => {
-                            //             setTimeout(() => {
-                            //                 setData([...data, newData]);
-
-                            //                 resolve();
-                            //             }, 1000)
-                            //         }),
-                            //     onRowUpdate: (newData, oldData) =>
-                            //         new Promise((resolve, reject) => {
-                            //             setTimeout(() => {
-                            //                 const dataUpdate = [...data];
-                            //                 const index = oldData.tableData.id;
-                            //                 dataUpdate[index] = newData;
-                            //                 setData([...dataUpdate]);
-
-                            //                 resolve();
-                            //             }, 1000)
-                            //         }),
-                            //     onRowDelete: oldData =>
-                            //         new Promise((resolve, reject) => {
-                            //             setTimeout(() => {
-                            //                 const dataDelete = [...data];
-                            //                 const index = oldData.tableData.id;
-                            //                 dataDelete.splice(index, 1);
-                            //                 setData([...dataDelete]);
-
-                            //                 resolve()
-                            //             }, 1000)
-                            //         }),
-                            // }}
+                            
                             />
+
+                        {showEditRole &&
+                            <EditRole
+                            dialog={()=>setshowEditRole(!showEditRole)}
+                            
+                            />
+                        } 
+
                         </ThemeProvider>
                         {showDialog &&
                             <CreateMember
@@ -235,12 +212,7 @@ const Members = () => {
 
             }
 
-            {showEditRole &&
-                <EditRole
-                    dialog={()=>setShowAssignee(!showEditRole)}
-                   
-                />
-            }   
+             
 
         </Wrapper>
     )
