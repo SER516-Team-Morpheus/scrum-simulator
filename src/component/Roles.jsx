480b59e7eeb4062c7f613df171eb3eb1c2682968
naheer <nnolas33@asu.edu>
2023-04-21 18:50:33 -0700

    added delete button to delete roles

--- src/component/Roles.jsx
+++ src/component/Roles.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+//import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect, forwardRef } from 'react';
 import styled from 'styled-components';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
@@ -6,7 +7,24 @@ import Cookies from 'js-cookie';
 import { createRoles, getRoles } from '../apis';
 import { ColorRing } from 'react-loader-spinner';
 import CreateRoles from './CreateRoles';
-
+import { ThemeProvider, createTheme, } from '@mui/material';
+import Search from '@material-ui/icons/Search';
+import MaterialTable from 'material-table';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowDownward from '@material-ui/icons/ArrowDownward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+//import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
 const Wrapper = styled.div`
 .heading-bar {
     display:flex;
@@ -53,6 +71,25 @@ top:50%;
 left: 50%;
 transform : translate(-40%, -70%)
 `;
+const tableIcons = {
+    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
+    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
 
 const Roles = ({ showItem }) => {
     let username = Cookies.get('username');
@@ -62,7 +99,23 @@ const Roles = ({ showItem }) => {
     const [RoleList, setRoleList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isCreateLoader, setIsCreateLoader] = useState(false);
-
+    const defaultMaterialTheme = createTheme();
+    const columns = [
+       {
+          title: 'Role',
+          field: 'role'
+        },
+        {
+          title: 'Delete Role',
+          field: 'delete',
+          render: rowData => (
+            <button onClick={() => handleDelete(rowData)}>Delete</button>
+          )
+        }
+      ];
+      function handleDelete(rowData) {
+        // Implement the code to delete the role
+      }
     const handleDialogRoles = () => {
         setShowDialogRoles(!showDialogRoles);}
     // const addRoles = (data) => {
@@ -119,9 +172,15 @@ const Roles = ({ showItem }) => {
                         colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
                     />
                     :
-                    <div>
+                    <div className="table">
                         {
-                            RoleList.length > 0 ? (
+                            <ThemeProvider theme={defaultMaterialTheme}>
+                            <MaterialTable
+                                icons={tableIcons}
+                                title=""
+                                columns={columns}
+                                data={RoleList}
+                            /*RoleList.length > 0 ? (
                                 RoleList.map(data => (
                                     <div className="role-list">
                                         <Typography className="heading" variant="h6" gutterBottom>{data.roleName}</Typography>
@@ -133,7 +192,9 @@ const Roles = ({ showItem }) => {
                                 <>
                                  
  
-                                </>
+                                </>*/
+                                 />
+                        </ThemeProvider>
 
                         }
                         {showDialogRoles &&
