6601f42ffc3f00bd3862d1b01151a5f735cf268d
naheer <nnolas33@asu.edu>
2023-04-21 17:13:50 -0700

    fetch the roles associated with  specified project

--- src/component/Roles.jsx
+++ src/component/Roles.jsx
@@ -1,29 +1,16 @@
-import React, { useState, useEffect, forwardRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import Cookies from 'js-cookie';
-import { getMembers, getRoles } from '../apis';
-import { ColorRing } from 'react-loader-spinner';
+import projectImg from '../img/project-img.jpg';
 import CreateRoles from './CreateRoles';
-import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-
+import Link from '@mui/material/Link';
+import { useNavigate } from "react-router-dom";
+import Cookies from 'js-cookie';
+import { createRoles, getRoles } from '../apis';
+import { ColorRing } from 'react-loader-spinner';
+//import { getRole } from '@testing-library/react';
 const Wrapper = styled.div`
-
 .heading-bar {
     display:flex;
     justify-content: space-between;
@@ -33,12 +20,10 @@ const Wrapper = styled.div`
     // margin-top: 10px;
     color: #8C1D40;
 }
-
 .create-btn {
     margin-top:10%;
     background-color: #8C1D40;
 }
-
 img {
     height:300px;
     width: 500px;
@@ -60,72 +45,61 @@ img {
     top:50%;
     left:30%;
 }
-.table {
-    margin-top:30px;
-}
+`;
+const CreateDialog = styled.div`
 
-
+height: 400px;
+width:600px;
+background-color: #d7dbd8;
+position:absolute;
+top:50%;
+left: 50%;
+transform : translate(-40%, -70%)
 `;
 
-
-
-const tableIcons = {
-    Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-    Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-    Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-    DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-    Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-    Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-    FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-    LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-    NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-    PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-    ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-    Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-    SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-    ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-    ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
-
-const Roles = () => {
+const Roles = ({ showItem }) => {
     let username = Cookies.get('username');
-    let password = Cookies.get('password');
-    let projectName = Cookies.get('projectName');
-    let projectId = Cookies.get('projectId')
-    const [showDialogRoles, setShowDialogRoles] = useState(false);
-    const [RoleList, setRoleList] = useState([]);
+    let password = Cookies.get('password') || 'testuser';
+    let projectName=Cookies.get('projectName');
+    const [showDialog, setShowDialog] = useState(false);
+    const navigate = useNavigate();
     const [isLoading, setIsLoading] = useState(true);
-    const [roleData, setRoleData] = useState([]);
-    const [columns, setColumns] = useState([
-        { title: 'Name', field: 'full_name' },
-        { title: 'Email', field: 'email' },
-        {
-            title: 'Role', field: 'role'
-        }
-    ]);
-    const handleDialogRoles = () => {
-        setShowDialogRoles(!showDialogRoles);}
-    const addRoles = (data) => {
-        setRoleList(prevState => [...prevState, data])
-    }
+    const [isCreateLoader, setRolesLoading] = useState(false);
+    const [RoleList, setRoleList] = useState([]);
 
+    const storeRole = (data) => {
+        setRoleList([data]);
+        setShowDialog(false);
+    }
+    const handleDialog = () => {
+        setShowDialog(!showDialog);
+    }
+    /*const selectRole = (roleName, projectName) => {
+        Cookies.set('roleName', roleName)
+        Cookies.set('projectName',projectName)
+        navigate('/roles/' + Cookies.get('roleName'))
+        navigate('/backlog')
+        showItem();
+    }*/
+    const createNewRole = (roleName, projectName) => {
+        setRolesLoading(true);
+        createRoles(username, password, roleName, projectName)
+          .then(res => {
+            const newRole = res.data.roles;
+            setRoleList(prevState => [...prevState, newRole]);
+            setRolesLoading(false);
+            setShowDialog(false);
+          })
+          .catch(function (error) {
+            setRolesLoading(false);
+          })
+    }
+    
     useEffect(() => {
-        getMembers(username, password, projectId)
+        getRoles(username,password,projectName)
             .then(res => {
-                
-                setRoleList(res.data.data)
+                setRoleList(res.data.roles.roleName)
                 setIsLoading(false);
-                getRoles(username, password, projectName)
-                    .then(roleData => {
-                        setRoleData(roleData.roles)
-                        setIsLoading(false);
-                    })
-                    .catch(function (error) {
-                        setIsLoading(false);
-                    })
             })
             .catch(function (error) {
                 setIsLoading(false);
@@ -139,8 +113,7 @@ const Roles = () => {
                     Roles
                 </Typography>
                 <div>
-                    <Button className="create-btn" variant="contained" onClick={() => setShowDialogRoles(true)}>Add roles</Button>
-
+                    <Button className="create-btn" variant="contained" onClick={() => setShowDialog(true)}>Create Role</Button>
                 </div>
             </div>
             {
@@ -155,21 +128,37 @@ const Roles = () => {
                         colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
                     />
                     :
-                    <div className="table">
-                        {showDialogRoles &&
+                    <div>
+                        {
+                            RoleList.length > 0 ? (
+                                RoleList.map(data => (
+                                    <div className="role-list">
+                                        <Typography className="heading" variant="h6" gutterBottom><Link onClick={() => selectRole(data.name,data.id)}>{data.name}</Link></Typography>
+                                        <Typography className="heading" variant="h9" gutterBottom>{data.description}</Typography>
+                                    </div>
+                                ))
+                            )
+                                :
+                                <>
+                                 
+ 
+                                </>
+
+                        }
+                        {showDialog &&
                             <CreateRoles
-                                dialog={handleDialogRoles}
-                                addRoles={addRoles}
+                                dialog={handleDialog}
+                                createNewRole={createNewRole}
+                                //newRole={'Roles'}
+                                setRolesLoading={setRolesLoading}
                             />
                         }
+
                     </div>
 
             }
-
-
-
         </Wrapper>
+
     )
 }
-
 export default Roles;
